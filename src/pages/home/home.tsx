0d064897c9f5b80ebe37c0e90a7c88455663c53e
Sebastián García Correa <sebastian435622@gmail.com>
2023-10-12 14:10:00 -0500

    Primer Commit

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,16 @@
+
+
+const Home = () => {
+  return (
+    <>
+    <div>home</div>
+    <p>Funciona hpt</p>
+
+    <a href="/si">asdasfd</a>
+
+    <p>Moisés Es Bien Gei</p>
+    </>
+  )
+}
+
+export default Home
