89ab74c1833c5e8203496006ecef1da2824344b5
Sebastián García Correa <sebastian435622@gmail.com>
2024-01-10 18:06:05 -0500

    Avanzando

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,27 +1,74 @@
-import React from "react";
-import style from "./nav.module.css";
-import PrincipalImg from "../../css/PorkProsper.png";
-import { IoLogoWhatsapp } from "react-icons/io"
-import { BsFacebook } from "react-icons/bs"
+import { useState } from 'react';
+import PrincipalImg from '../../css/PorkProsper.png'; // replace with your actual image path
+import style from './nav.module.css';
+import { FaUserCircle  } from 'react-icons/fa';
 
 const Nav = () => {
   return (
-    <>
-      <main className={style.main}>
-        <section className={style.sectionUp}>
-            <div className={style.contactNumbers}>
-              <p><IoLogoWhatsapp />+57 310 746 5499</p>
-              <p><BsFacebook />PorkProsper</p>
-            </div>
-              <img src={PrincipalImg} alt="Logo" className={style.PrincipalImg}/>
-            <div className={style.AttentionHours}>
-              <p>Every day bringing the best proucts for you!</p>
+    <main>
+      <nav className={style.nav}>
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+        </ul>
+        <ul>
+          <li className={style.dropdown}>
+          <DropDownItem text={"Redes"} />
 
-            </div>
-        </section>
-      </main>
-    </>
+          </li>
+          <li className={style.logo}>
+            <img src={PrincipalImg} alt="" className={style.logo}/>
+          </li>
+          <li className={style.dropdown}>
+            <DropDownItem text={"Contacto"} />
+          </li>
+        </ul>
+        <ul >
+          <li className={style.dropdown}>
+            <span className={style.userIcon}><FaUserCircle /></span>
+          </li>
+        </ul>
+      </nav>
+    </main>
   );
 };
+
+const DropDownItem = (props: any) => {
+
+  const openFacebook = () => {
+    window.open("https://www.facebook.com", "_blank");
+  }
+  const openInstagram = () => {
+    window.open("https://www.instagram.com", "_blank");
+  }
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (props.text === "Redes") {
+    return (
+      <div onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
+        <a>{props.text}</a>
+        {isOpen && (
+          <ul>
+            <li><a href="" onClick={openFacebook}>Facebook</a></li>
+            <li><a href="" onClick={openInstagram}>Instagram</a></li>
+          </ul>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    
+    <div onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
+      <a>{props.text}</a>
+      {isOpen && (
+        <ul>
+          <li><a href="#">+57 123456789</a></li>
+        </ul>
+      )}
+    </div>
+  );
+}
 
 export default Nav;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,12 +3,6 @@ import style from "./home.module.css";
 const Home = () => {
   return (
     <>
-      <main className={style.main}>
-        <div>home</div>
-        <p>Funciona hpt</p>
-
-        <a href="/si">asdasfd</a>
-      </main>
     </>
   );
 };
